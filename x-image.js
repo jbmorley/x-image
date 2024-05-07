fdb5ef2c73d52b518077dd7f4d2ed3dca5fcb1ee
Jason Morley <hello@jbmorley.co.uk>
2024-05-07 07:44:09 -1000

    Update with performance improvements from jbmorley.co.uk

--- x-image.js
+++ x-image.js
@@ -7,13 +7,28 @@ class XImage extends HTMLElement {
         }
 
         this.element = null;
+        this.selection = null;
+
+        this.width = Number(this.getAttribute("width"));
+        this.height = Number(this.getAttribute("height"));
+        this.aspectRatio = this.width / this.height;
 
         this.mutationObserver = new MutationObserver(this.onMutation.bind(this));
         this.mutationObserver.observe(this, {
           childList: true
         });
 
-        this.resizeObserver = new ResizeObserver(this.onResize.bind(this));
+        function debounce(func, time) {
+            var timer;
+            return function(event){
+                if (timer) {
+                    clearTimeout(timer);
+                }
+                timer = setTimeout(func, time, event);
+            };
+        }
+
+        this.resizeObserver = new ResizeObserver(debounce(this.onResize.bind(this), 100));
         this.resizeObserver.observe(this);
 
         this.sources = [];
@@ -25,11 +40,10 @@ class XImage extends HTMLElement {
         for (const mutation of mutations) {
             for (const node of mutation.addedNodes) {
                 if (node.tagName == "X-SOURCE") {
-                    console.log("Adding source...")
                     this.sources.push({
                         src: node.getAttribute("src"),
                         width: Number(node.getAttribute("width")),
-                        height: Number(node.getAttribute("width")),
+                        height: Number(node.getAttribute("height")),
                     });
                     needsUpdate = true;
                 }
@@ -53,22 +67,37 @@ class XImage extends HTMLElement {
     }
 
     selectSource() {
+        const scale = window.devicePixelRatio;
         const bounds = this.getBoundingClientRect();
+        const targetWidth = bounds.width * scale;
+        const targetHeight = targetWidth / this.aspectRatio;
+
+        // Don't do anything if there are no sources.
+        if (this.sources.length < 1) {
+            return;
+        }
 
         // Determine the correct entry.
         var candidate = null
         for (const source of this.sources) {
-            if (candidate == null || source.width >= bounds.width) {
+            if (candidate == null || source.width >= targetWidth) {
                 candidate = source
             } else {
                 break;
             }
         }
 
+        if (this.selection === candidate) {
+            this.element.width = Math.min(bounds.width, candidate.width);
+            this.element.height = this.element.width / this.aspectRatio;
+            return;
+        }
+
         // Create the new element.
         var element = document.createElement("img");
         element.src = candidate.src;
         element.width = Math.min(bounds.width, candidate.width);
+        element.height = element.width / this.aspectRatio;
 
         // Replace or append as necessary.
         if (this.element == null) {
@@ -77,6 +106,7 @@ class XImage extends HTMLElement {
             this.element.replaceWith(element);
         }
         this.element = element;
+        this.selection = candidate;
     }
 
     disconnectedCallback() {
