7b559b68e23e2ac28a7f032de8e17899e7259759
Jason Morley <hello@jbmorley.co.uk>
2024-12-08 14:53:45 -1000

    feat: Support upgrading standard `img` tags

--- x-image.js
+++ x-image.js
@@ -1,8 +1,7 @@
 class XImage extends HTMLElement {
 
+    // TODO: Initialize before connected so it can be created dynamically.
     connectedCallback() {
-
-        console.log("Connected!");
 
         if (!this.isConnected) {
             return;
@@ -125,3 +124,55 @@ class XImage extends HTMLElement {
 }
 
 customElements.define('x-image', XImage);
+
+function promoteImg(img) {
+    if (img.getAttribute("x-srcset") === null) {
+        return
+    }
+    const srcset = img.getAttribute("x-srcset")
+    const sources = srcset.split(",").map((source) => {
+        const components = source.trim().split(/\s+/);
+        return {src: components[0], width: parseInt(components[1]), height: parseInt(components[2])};
+    });
+
+    const xImage = document.createElement('x-image');
+    xImage.setAttribute("width", img.width);
+    xImage.setAttribute("height", img.height);
+    img.replaceWith(xImage);  // TODO: Work out how to push this later?
+    xImage.add(sources.map((source) => {
+        const element = document.createElement('x-source');
+        element.setAttribute("src", source.src);
+        element.setAttribute("width", source.width);
+        element.setAttribute("height", source.height);
+        return element;
+    }));
+}
+
+// Promote img tags with the `x-srcset` attribute to x-image tags.
+document.querySelectorAll('img').forEach(img => {
+    promoteImg(img);
+});
+
+
+function replaceImages(node) {
+    if (node.tagName === 'IMG') {
+        promoteImg(node);
+    } else if (node.querySelectorAll) {
+        node.querySelectorAll('img').forEach(img => {
+            promoteImg(img);
+        });
+    }
+}
+
+// Set up the MutationObserver for images.
+const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => replaceImages(node));
+    });
+});
+
+// Start observing the document for changes
+observer.observe(document, {
+    childList: true,  // Observe direct child additions/removals
+    subtree: true     // Observe changes in all descendants
+});
